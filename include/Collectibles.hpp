0d993be6097b6ec7ad941051f925df1f3f21233b
Farid Karadsheh <karadshf@arch08.cis.gvsu.edu>
2018-10-02 20:53:44 -0400

    Update and power up accept person reference

--- include/Collectibles.hpp
+++ include/Collectibles.hpp
@@ -7,17 +7,17 @@
 
 class Collectibles {
 	// Detects collision with a player. Has no effect on monsters.
-	virtual bool detectCollision(Person person);
+	virtual bool detectCollision(Person &person);
 
 	// If the collectible is stationary then this method does nothing.
 	virtual void move();
 	
 	// When colliding with player, activate the collectibles powers.
-	virtual void activate();
+	virtual void activate(Person &person);
 
 	// Will be called each frame. Checks flags, calls move(), and \
 	activates if neccessary.
-	virtual void update();
+	virtual void update(Person &person);
 
 protected:
 	// Coordinates and Dimensions
@@ -32,4 +32,8 @@ protected:
 	
 	// Flag - is true when collectible is colliding with player.
 	bool isColliding;
+
+	// time management
+	const int POWER_UP_TIME = 10;
+	int elapsed time;
 }
