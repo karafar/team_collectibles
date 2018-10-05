e9135f89a32b595e4d0b3fe5028370f761216610
Farid Karadsheh <karadshf@arch05.cis.gvsu.edu>
2018-10-04 21:44:43 -0400

    Pushed methods in OrangePotion to Collectilbles.hpp

--- OrangePotion.cpp
+++ OrangePotion.cpp
@@ -5,78 +5,22 @@
 class OrangePotion: public Collectibles {
 public:
 	OrangePotion()
-	: Collectibles()
-	 {
-		loadSprite();	
-		sprite.setTexture(texture);
-		sf::FloatRect spriteSize = sprite.getGlobalBounds();
-		sprite.setOrigin(spriteSize.width/2.0, spriteSize.height/2.0);
-		sprite.setPosition(x,y);
+	{
+		loadSprite("sprites/orangePoition.png");	
 	}
 
 	OrangePotion(int x, int y)
 	: Collectibles()
 	{
-		loadSprite();
-		sprite.setTexture(texture);
-		sf::FloatRect spriteSize = sprite.getGlobalBounds();
-		sprite.setOrigin(spriteSize.width/2.0, spriteSize.height/2.0);
 		this->x = x;
 		this->y = y;
-		sprite.setPosition(this->x,this->y);
+		loadSprite("sprites/orangePoition.png");
 	}
 
-	bool detectCollision(Person &person) {
-		return Collision::BoundingBoxTest(person.getSprite(), this->sprite);
-	}
-
-	sf::Sprite getSprite() {
-		return this->sprite;
-	}
-
-	void move(){
-		sf::Time time = clock.getElapsedTime();
-		sf::Int32 mills = time.asMilliseconds();
-		if(mills % 1000 > 500) {
-			y += bounce;
-			sprite.setPosition(x, y);
-			bounce *= -1;
-			clock.restart();
-		}
-	}
 	void activate(Person &person){
 		int health = person.getHealth();
 		if(health <= 50) person.setHealth(health+50);
 		else person.setHealth(100);
 		hasActivated = true;
 	}
-
-	void loadSprite(){
-		if(!texture.loadFromFile("sprites/orangePoition.png"))
-		{
-			printf("Failed to load texture:  orangePotion.png\n");
-			exit(EXIT_FAILURE);
-		}		
-	}
-
-	void update(Person &person){
-		if(!hasActivated)
-			if(detectCollision(person))
-				activate(person);
-		move();	
-	}
-
-	void render(sf::RenderWindow &window) {
-		if(!hasActivated)
-			window.draw(sprite);
-	}
-
-private:
-//	sf::Sprite sprite;
-//	sf::Texture texture;
-//	int x = 300;
-//	int y = 300;	
-//	int bounce = 1;
-//	bool hasActivated = false;
-//	sf::Clock clock;
 };
--- include/Collectibles.hpp
+++ include/Collectibles.hpp
@@ -1,31 +1,81 @@
-//TODO: add info here.
+/**
+	Class: Collectibles
+	Purpose: Acts as a parent to specific colllectible items. This class
+	is not intended to be instantiated.
 
+	Made for CIS343 @ GVSU Fall 2018
+	Team Members:
+	Isfar Baset
+	Allision Bickford
+	Nick Cone
+	Farid Karadsheh
+
+*/
 #ifndef H__COLLECTIBLES__
 #define H__COLLECTIBLES__
 
 #include "SFML/Graphics.hpp"
 #include "SFML/System/Clock.hpp"
 #include "SFML/Window.hpp"
+#include "Collision.hpp"
 #include "Person.hpp"
+#include <string.h>
+#include <stdio.h>
 
 class Collectibles {
 public:
-	Collectibles(){}
-	// Detects collision with a player. Has no effect on monsters.
-	virtual bool detectCollision(Person &person) = 0;
 
-	// If the collectible is stationary then this method does nothing.
-	virtual void move() = 0;
+	// Detects collision with a player.
+	bool detectCollision(Person &person) {
+		return Collision::BoundingBoxTest(person.getSprite(), this->sprite);
+	}
+
+	// Moves the collectible up and down.
+	void move() {
+		sf::Time time = clock.getElapsedTime();
+		sf::Int32 mills = time.asMilliseconds();
+		if(mills % 1000 > 500) {
+			y += bounce;
+			sprite.setPosition(x, y);
+			bounce *= -1;
+			clock.restart();
+		}
+	}
 	
-	// When colliding with player, activate the collectibles powers.
+	// When colliding with player, activate the collectibles powers.\
+	Collision detection is not handled here.
 	virtual void activate(Person &person) = 0;
 
 	// Loads sprite to sprite field.
-	virtual void loadSprite() = 0;
+	void loadSprite(const std::string filename) {
+		if(!texture.loadFromFile(filename)) {
+			printf("Failed to load texture: %s\n", filename.c_str());
+			exit(EXIT_FAILURE);
+		}
+		sprite.setTexture(texture);
+		sf::FloatRect spriteSize = sprite.getGlobalBounds();
+		sprite.setOrigin(spriteSize.width/2.0, spriteSize.height/2.0);
+		sprite.setPosition(x,y);
+	}
 	
 	// Will be called each frame. Checks flags, calls move(), and \
 	activates if neccessary.
-	virtual void update(Person &person) = 0;
+	void update(Person &person) {
+		if(!hasActivated)
+			if(detectCollision(person))
+				activate(person);
+		move();
+	}
+
+	// Recieves game's window, then draws the collectible.
+	void render(sf::RenderWindow &window) {
+		if(!hasActivated)
+			window.draw(sprite);
+	}
+
+	sf::Sprite getSprite() {
+		return this->sprite;
+	}
 
 protected:
 	sf::Sprite sprite;
